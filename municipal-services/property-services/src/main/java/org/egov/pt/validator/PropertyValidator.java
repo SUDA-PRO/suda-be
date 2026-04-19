161023fce0375f46b875898a2697abe1e18fcff4
sudacgpoc <sudacgpoc@gmail.com>
2026-04-19 20:39:25 +0530

    Update PropertyValidator.java
    line 286

--- municipal-services/property-services/src/main/java/org/egov/pt/validator/PropertyValidator.java
+++ municipal-services/property-services/src/main/java/org/egov/pt/validator/PropertyValidator.java
@@ -283,7 +283,7 @@ public class PropertyValidator {
 
 		validateInstitution(property, errorMap);
 		//Developer Need to Fix
-		//Map<String, List<String>> codes = propertyUtil.getAttributeValues(tenantId, PTConstants.MDMS_PT_MOD_NAME, masterNames,
+		//Map<String, List<String>> codes = propertyUtil.getAttributeValues(tenantId, PTConstants.MDMS_PT_MOD_NAME, masterNames;
 				"$.*.code", PTConstants.JSONPATH_CODES, request.getRequestInfo());
 		
 		if (null != codes) {
